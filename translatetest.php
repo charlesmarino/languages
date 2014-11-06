7ed964c2c321b70a2eb0eea4c9beab54b8b33c46
Charles <charlesmarino1@gmail.com>
2014-11-05 22:11:22 -0500

    updating test url

--- translatetest.php
+++ translatetest.php
@@ -7,7 +7,7 @@ header('Content-Type:application/json; charset=UTF-8');
 	$encoded_words = urlencode($english_words);
 
 	// Get the specific student data
-	$translated_words = file_get_contents('http://localhost/translateme.php?action=translate&english_words=' . $encoded_words . '&language=danish');
+	$translated_words = file_get_contents('http://localhost/languages/translateme.php?action=translate&english_words=' . $encoded_words . '&language=danish');
 	echo "test";
 	echo $translated_words;
 
